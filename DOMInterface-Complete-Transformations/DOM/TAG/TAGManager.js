919ab4daf50e40399aa83e3f30661e120d6860e9
Ike-l <76500816+Ike-l@users.noreply.github.com>
2024-02-15 21:52:43 +0000

    Delete DOMInterface-Complete-Transformations directory
    Not needed

--- DOMInterface-Complete-Transformations/DOM/TAG/TAGManager.js
+++
@@ -1,6 +0,0 @@
-import Element from "./Parent/Element.js"
-import Canvas from "./Child/Canvas.js"
-import Button from "./Child/Button.js"
-import Slider from "./Child/Slider.js"
-
-export { Element, Canvas, Button, Slider }
